036ea01328b9be57a160cfed7dea8a9b2751f8e4
giveMeLife <j.fernandez.munoz@hotmail.com>
2019-04-30 11:41:37 -0400

    timestamp para metodo Post Actor

--- src/main/java/com/grupo4/tarea_1/services/ActorService.java
+++ src/main/java/com/grupo4/tarea_1/services/ActorService.java
@@ -1,10 +1,13 @@
 package com.grupo4.tarea_1.services;
 
+import com.grupo4.tarea_1.models.Film;
 import com.grupo4.tarea_1.models.Actor;
 import com.grupo4.tarea_1.repositories.ActorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.sql.Timestamp;
 
 @RestController
 @RequestMapping("/actors")
@@ -29,6 +32,19 @@ public class ActorService {
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
     public Actor create(@RequestBody Actor resource) {
+        Timestamp time = new Timestamp(System.currentTimeMillis());
+        resource.setLastUpdate(time);
+        System.out.println(resource.getLastUpdate());
         return actorRepository.save(resource);
     }
+
+    @RequestMapping(value = "/{id}/films", method = RequestMethod.GET)
+    @ResponseBody
+    public  List<Film> findFilm(@PathVariable("id") Integer id) {
+        Actor actor = actorRepository.findActorByActorId(id);
+        List<Film> films = actor.getFilms();
+        return films;
+
+    }
+
 }
